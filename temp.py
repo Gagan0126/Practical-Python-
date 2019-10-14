52e76018c0a25b56a5557e6567d5f469a3d1dfb3
Gagan0126 <56566287+Gagan0126@users.noreply.github.com>
2019-10-15 04:39:49 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,41 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)# Creating a List 
+List = [] 
+print("Intial blank List: ") 
+print(List) 
+  
+# Creating a List with  
+# the use of a String 
+List = ['GeeksForGeeks'] 
+print("\nList with the use of String: ") 
+print(List) 
+  
+# Creating a List with 
+# the use of multiple values 
+List = ["Geeks", "For", "Geeks"] 
+print("\nList containing multiple values: ") 
+print(List[0])  
+print(List[2]) 
+  
+# Creating a Multi-Dimensional List 
+# (By Nesting a list inside a List) 
+List = [['Geeks', 'For'] , ['Geeks']] 
+print("\nMulti-Dimensional List: ") 
+print(List) 
+
+
+# OUTPUT
+
+['apple', 'banana', 'cherry']
+Intial blank List: 
+[]
+
+List with the use of String: 
+['GeeksForGeeks']
+
+List containing multiple values: 
+Geeks
+Geeks
+
+Multi-Dimensional List: 
+[['Geeks', 'For'], ['Geeks']]
