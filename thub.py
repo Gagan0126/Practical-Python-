0fe880c6e8037cc25ad5e42275da710d4e018504
Gagan0126 <56566287+Gagan0126@users.noreply.github.com>
2019-10-15 04:36:10 +0530

    Add files via upload

---
+++ thub.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+thislist=['apple','banana','grapes']
+
+
+# In[2]:
+
+
+thislist.pop()
+
+
+# In[3]:
+
+
+thislist.insert(1,'strawbeery')
+
+
+# In[4]:
+
+
+thislist
+
+
+# In[5]:
+
+
+thislist.pop(2)
+
+
+# In[6]:
+
+
+thislist
+
+
+# In[7]:
+
+
+thislist.append("manga")
+
+
+# In[8]:
+
+
+thislist
+
+
+# In[9]:
+
+
+reverse(thislist)
+
+
+# In[10]:
+
+
+index(thislist)
+
+
+# In[15]:
+
+
+thislist.reverse()
+
+
+# In[16]:
+
+
+thislist
+
+
+# In[17]:
+
+
+thislist.sort()
+
+
+# In[18]:
+
+
+thislist
+
+
+# In[22]:
+
+
+thislist.index('apple')
+
+
+# In[23]:
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+
+# In[24]:
+
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+
+# In[27]:
+
+
+# the last item of the list:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+
+# In[28]:
+
+
+# Creating a List 
+List = [] 
+print("Intial blank List: ") 
+print(List) 
+  
+# Creating a List with  
+# the use of a String 
+List = ['GeeksForGeeks'] 
+print("\nList with the use of String: ") 
+print(List) 
+  
+# Creating a List with 
+# the use of multiple values 
+List = ["Geeks", "For", "Geeks"] 
+print("\nList containing multiple values: ") 
+print(List[0])  
+print(List[2]) 
+  
+# Creating a Multi-Dimensional List 
+# (By Nesting a list inside a List) 
+List = [['Geeks', 'For'] , ['Geeks']] 
+print("\nMulti-Dimensional List: ") 
+print(List) 
+
+
+# In[ ]:
+
+
+
+
